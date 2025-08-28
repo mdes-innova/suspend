80e6d2df4acd8f584a17b959b77558a76312a93f
Arnon Songmoolnak <arnon3339@gmail.com>
2025-08-28 19:57:45 +0700

    checking access for thaiid

--- frontend/app/(any)/thaiid/page.tsx
+++ frontend/app/(any)/thaiid/page.tsx
@@ -1,3 +1,4 @@
+import { getAccess } from '@/app/(main)/page';
 import { redirect } from 'next/navigation'
 
 export default async function Page({
@@ -6,11 +7,23 @@ export default async function Page({
   searchParams: Promise<{ pathname: string }>
 }) {
   const { pathname } = await searchParams;
+  const goToPath = pathname === '/thaiid'? '/': pathname;
+  const access = await getAccess();
+  const baseUrl = process.env.NODE_ENV === "development"? process.env.BACKEND_URL_DEV: process.env.BACKEND_URL_PROD;
+  if (access) {
+    const resAccessMe = await fetch(`${baseUrl}/user/users/me/`, {
+      headers: {
+          Authorization: `Bearer ${access}`
+        },
+    }); 
+
+    if (resAccessMe.ok) redirect(goToPath);
+  }
   const thaiidEndpoint = process.env.THAIID_ENDPOINT;
   const clientId = process.env.THAIID_CLIENT_ID;
   const redirectUri = process.env.THAIID_CALLBACK;
   const scope = 'given_name%20family_name%20birthdate'
-  const state = pathname;
+  const state = goToPath;
 
   const url = `${thaiidEndpoint}/?response_type=code&` +
       `client_id=${clientId}&redirect_uri=${redirectUri}&` +
@@ -21,7 +34,8 @@ export default async function Page({
   })
 
   if (res.status != 200) {
-    redirect(`/login?pathname=${encodeURIComponent(pathname)}`)
+    console.log(res);
+    redirect(`/login?pathname=${encodeURIComponent(goToPath)}`)
   }
 
   redirect(res.url);
