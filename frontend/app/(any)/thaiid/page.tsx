ad3a6ce9800d8a477d89266f8abfc5c2012ecd4a
arnon3339 <arnon3339@gmail.com>
2025-08-29 08:38:56 +0700

    remove get access in thaiid

--- frontend/app/(any)/thaiid/page.tsx
+++ frontend/app/(any)/thaiid/page.tsx
@@ -7,36 +7,24 @@ export default async function Page({
   searchParams: Promise<{ pathname: string }>
 }) {
   const { pathname } = await searchParams;
-  const goToPath = pathname === '/thaiid'? '/': pathname;
-  const access = await getAccess();
-  const baseUrl = process.env.NODE_ENV === "development"? process.env.BACKEND_URL_DEV: process.env.BACKEND_URL_PROD;
-  if (access) {
-    const resAccessMe = await fetch(`${baseUrl}/user/users/me/`, {
-      headers: {
-          Authorization: `Bearer ${access}`
-        },
-    }); 
-
-    if (resAccessMe.ok) redirect(goToPath);
-  }
   const thaiidEndpoint = process.env.THAIID_ENDPOINT;
   const clientId = process.env.THAIID_CLIENT_ID;
   const redirectUri = process.env.THAIID_CALLBACK;
   const scope = 'given_name%20family_name%20birthdate'
-  const state = goToPath;
+  const state = pathname;
 
   const url = `${thaiidEndpoint}/?response_type=code&` +
       `client_id=${clientId}&redirect_uri=${redirectUri}&` +
       `scope=${scope}&state=${encodeURIComponent(state)}`;
-    
+  
+  console.log(url);
   const res = await fetch(url, {
     headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
   })
 
   if (res.status != 200) {
-    console.log(res);
-    redirect(`/login?pathname=${encodeURIComponent(goToPath)}`)
+    redirect(`/login?pathname=${encodeURIComponent(pathname)}`)
+  } else {
+    redirect(res.url);
   }
-
-  redirect(res.url);
 }
